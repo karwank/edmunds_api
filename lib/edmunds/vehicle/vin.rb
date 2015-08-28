de8199f1a017f7ba3fd5167425e19a443929224b
Karol <karol.karwacki@polcode.net>
2015-08-28 12:16:51 +0200

    quick fix

--- lib/edmunds/vehicle/vin.rb
+++ lib/edmunds/vehicle/vin.rb
@@ -3,7 +3,7 @@ module Edmunds
   class Vin < Vehicle
 
     # http://developer.edmunds.com/api-documentation/vehicle/spec_vin_decoding/v2/02_by_vin/api-description.html
-    def vin vin
+    def vin vin, params = {}
       @parameters = []
       call_api "/vins/#{vin}?", params
     end
